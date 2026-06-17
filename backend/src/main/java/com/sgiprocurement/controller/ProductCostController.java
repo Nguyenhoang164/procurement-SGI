0cb3f2ae3070d6d843c577b191bd9dfaf9df3a8c
Nguyenhoang164 <Kamito062004@gmail.com>
2026-06-17 09:33:20 +0700

    fix: add missing RequestParam import to ProductCostController

--- backend/src/main/java/com/sgiprocurement/controller/ProductCostController.java
+++ backend/src/main/java/com/sgiprocurement/controller/ProductCostController.java
@@ -6,12 +6,12 @@ import com.sgiprocurement.service.ProductCostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
